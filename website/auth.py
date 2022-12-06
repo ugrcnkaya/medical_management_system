1706478acacf0df34d3436da73a9bb9a62d9099a
Ugurcan Kaya <118749864+ugrcnkaya@users.noreply.github.com>
2022-12-06 23:46:43 +0100

    Initial

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return '<p>Login</p>'
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -12,6 +12,6 @@ def logout():
 
 @auth.route('/sign_up')
 def sign_up():
-    return '<p>sign_up</p>'
+    return render_template("sign_up.html")
 
 
