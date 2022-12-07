a3bc2e3624c886ddafd6192b8b95963d4156d5b2
Ugurcan Kaya <118749864+ugrcnkaya@users.noreply.github.com>
2022-12-07 12:39:54 +0100

    Login Required for Home Page

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 ##routes for website
 from flask import Blueprint, render_template
+from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
     return render_template("home.html")
